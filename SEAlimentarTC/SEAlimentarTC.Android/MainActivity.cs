f96351a441437bd54a5e569af1f2d0e82d46393a
wfranco <wiliam.franco@meuresiduo.com>
2021-08-18 18:12:56 -0300

    test

--- SEAlimentarTC/SEAlimentarTC.Android/MainActivity.cs
+++ SEAlimentarTC/SEAlimentarTC.Android/MainActivity.cs
@@ -7,7 +7,6 @@ using Android.OS;
 
 namespace SEAlimentarTC.Droid
 {
-    //test
     [Activity(Label = "SEAlimentarTC", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
