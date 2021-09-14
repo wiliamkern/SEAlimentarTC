6d9695567fc629f217896a30600c47ad4894cd09
wfranco <wiliam.franco@meuresiduo.com>
2021-09-13 22:16:43 -0300

    modificações

---
+++ SEAlimentarTC/SEAlimentarTC/Dtos/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEAlimentarTC.Dtos
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public decimal Height { get; set; }
+        public bool Sex { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Imc { get; set; }
+    }
+}
---
+++ SEAlimentarTC/SEAlimentarTC/ViewModel/LoggedModel.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using SEAlimentarTC.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SEAlimentarTC.Model
+{
+    class LoggedModel : UserModel
+    {
+        public static User LoggedUser
+        {
+            get
+            {
+                return new User();
+            }
+            set
+            {
+              
+                
+            }
+        }
+    }
+}
