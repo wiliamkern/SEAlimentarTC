1ade0be0d026cf724b67577aeec606292745f290
wiliamkern <wiliam.franco@hotmail.com>
2021-11-07 19:15:43 -0300

    Inclusão loading no criar conta

--- SEAlimentarTC/SEAlimentarTC/View/Config.xaml.cs
+++ SEAlimentarTC/SEAlimentarTC/View/Config.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SEAlimentarTC.Dtos;
+﻿using Acr.UserDialogs;
+using SEAlimentarTC.Dtos;
 using SEAlimentarTC.Repository;
 using SEAlimentarTC.ViewModel;
 using System;
@@ -57,64 +58,73 @@ namespace SEAlimentarTC.View
             }
         }
 
-        private async void SaveData(object sender, EventArgs e)
+        private void SaveData(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(Age.Text) || string.IsNullOrEmpty(Name.Text) || string.IsNullOrEmpty(Weight.Text) || string.IsNullOrEmpty(Height.Text)
-                || (!SexM.IsChecked && !SexF.IsChecked))
-                await App.Current.MainPage.DisplayAlert("Atenção", "Você precisa preencher todos os campos para prosseguir", "OK");
-            else
+            User user = new User();
+
+            Device.BeginInvokeOnMainThread(() => UserDialogs.Instance.ShowLoading("Criando conta. Por favor aguarde!", MaskType.Black));
+            _ = Task.Run(async () =>
             {
-                User user = new User();
-                user.Age = !string.IsNullOrEmpty(Age.Text) ? Convert.ToInt32(Age.Text.Trim()) : 0;
-                user.Name = Name.Text;
-                user.Weight = !string.IsNullOrEmpty(Weight.Text) ? decimal.Parse(Weight.Text) : 0;
-                user.Height = !string.IsNullOrEmpty(Height.Text) ? decimal.Parse(Height.Text) : 0;
-                user.Sex = SexM.IsChecked ? "M" : "F";
-                user.Imc = System.Math.Round((decimal)(user.Weight / (user.Height * user.Height)), 2);
-
-                bool Updated = false;
-
-                if (LoggedModel.LoggedUser != null)
-                {
-                    user.UserID = LoggedModel.LoggedUser.UserID;
-                    user.UpdateDate = DateTime.Now;
-                    Updated = await new UserModel().UpdateUserAsync(user);
-                }
-
-                if (Updated)
-                    await App.Current.MainPage.DisplayAlert("", "Dados atualizados com sucesso!", "Fechar");
+                if (string.IsNullOrEmpty(Age.Text) || string.IsNullOrEmpty(Name.Text) || string.IsNullOrEmpty(Weight.Text) || string.IsNullOrEmpty(Height.Text)
+              || (!SexM.IsChecked && !SexF.IsChecked))
+                    await App.Current.MainPage.DisplayAlert("Atenção", "Você precisa preencher todos os campos para prosseguir", "OK");
                 else
                 {
-                    user.InsertDate = DateTime.Now;
-                    user.UserID = 1;
-                    user.ReceiveAlerts = true;
+                    user.Age = !string.IsNullOrEmpty(Age.Text) ? Convert.ToInt32(Age.Text.Trim()) : 0;
+                    user.Name = Name.Text;
+                    user.Weight = !string.IsNullOrEmpty(Weight.Text) ? decimal.Parse(Weight.Text) : 0;
+                    user.Height = !string.IsNullOrEmpty(Height.Text) ? decimal.Parse(Height.Text) : 0;
+                    user.Sex = SexM.IsChecked ? "M" : "F";
+                    user.Imc = System.Math.Round((decimal)(user.Weight / (user.Height * user.Height)), 2);
 
-                    if (new UserModel().InsertUseAsync(user))
+                    bool Updated = false;
+
+                    if (LoggedModel.LoggedUser != null)
                     {
-                        // faz o insert dos cardápios pré definidos
-                        if(new FoodMenuModel().GetList().Count == 0)
-                        {
-                            await new GenerateEnvironmentDefault().InsertAllFoodMenus1600KcalAsync();
-                            await new GenerateEnvironmentDefault().InsertAllFoodMenus1800KcalAsync();
-                            await new GenerateEnvironmentDefault().InsertAllFoodMenus2000KcalAsync();
-                            await new GenerateEnvironmentDefault().InsertAllFoodMenus2200KcalAsync();
-                            await new GenerateEnvironmentDefault().InsertAllFoodMenus2500KcalAsync();
-                        }
-
-                        await App.Current.MainPage.DisplayAlert("", "Dados salvos com sucesso!", "Fechar");
+                        user.UserID = LoggedModel.LoggedUser.UserID;
+                        user.UpdateDate = DateTime.Now;
+                        Updated = await new UserModel().UpdateUserAsync(user);
                     }
+
+                    if (Updated)
+                        await App.Current.MainPage.DisplayAlert("", "Dados atualizados com sucesso!", "Fechar");
                     else
-                        await App.Current.MainPage.DisplayAlert("", "Não foi possível salvar os dados", "Fechar");
+                    {
+                        user.InsertDate = DateTime.Now;
+                        user.UserID = 1;
+                        user.ReceiveAlerts = true;
+
+                        if (new UserModel().InsertUseAsync(user))
+                        {
+                            // faz o insert dos cardápios pré definidos
+                            if (new FoodMenuModel().GetList().Count == 0)
+                            {
+                                await new GenerateEnvironmentDefault().InsertAllFoodMenus1600KcalAsync();
+                                await new GenerateEnvironmentDefault().InsertAllFoodMenus1800KcalAsync();
+                                await new GenerateEnvironmentDefault().InsertAllFoodMenus2000KcalAsync();
+                                await new GenerateEnvironmentDefault().InsertAllFoodMenus2200KcalAsync();
+                                await new GenerateEnvironmentDefault().InsertAllFoodMenus2500KcalAsync();
+                            }
+
+                            await App.Current.MainPage.DisplayAlert("", "Dados salvos com sucesso!", "Fechar");
+                        }
+                        else
+                            await App.Current.MainPage.DisplayAlert("", "Não foi possível salvar os dados", "Fechar");
+                    }
                 }
+            }).ContinueWith(result => Device.BeginInvokeOnMainThread(async () =>
+            {
+                UserDialogs.Instance.HideLoading();
 
                 // carrega os dados atualizados pra sessão usuário
                 LoggedModel.LoggedUser = user;
 
-                if (!CheckImcIsValid(user))               
-                    await Navigation.PushAsync(new ImcNotValid());              
-                else                
-                    await Navigation.PushAsync(new MainPage());             
-            }
+                if (!CheckImcIsValid(user))
+                    await Navigation.PushAsync(new ImcNotValid());
+                else
+                    await Navigation.PushAsync(new MainPage());
+            }));           
+            
         }
 
         public bool CheckImcIsValid(User user)
